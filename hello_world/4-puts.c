8f9434cbe0783c78fd85e9fb40f0c0c84fcd3f31
edluca14 <6755@holbertonstudents.com>
2023-06-14 10:26:58 -0700

    task 4 i modified the lines inside the file 4-puts.c and finished to install betty in the system

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #!/bin/bash
-int main ()
-	puts ("Programming is like building a multilingual puzzle\n")
-	return (0)
+int main () {
+	puts ("Programming is like building a multilingual puzzle\n") ;
+	return (0) ;
+}

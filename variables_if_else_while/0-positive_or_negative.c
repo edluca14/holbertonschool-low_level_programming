2f233b03211e0ad827f2aa73716ab82441bd334c
edluca14 <6755@holbertonstudents.com>
2023-06-15 08:47:29 -0700

    solving writing problems in the notes of task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - This program will assign a random number
  * to the variable n each time it is executed.
  * Complete the source code in order to print

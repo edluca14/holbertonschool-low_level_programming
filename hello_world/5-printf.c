1da126684c29001e8eb0922ac98d66d3f17e44a4
edluca14 <6755@holbertonstudents.com>
2023-06-14 18:49:38 -0700

    add a description of main function and add a new line character

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Prints with proper grammar, but
+ *               the outcome is a piece of art,
+ *
+ * return: always 0.
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }

e589c9c26529398b664817fab0eee4b034fd04f0
edluca14 <6755@holbertonstudents.com>
2023-06-16 09:36:29 -0700

    task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar, followed by a new line
+ *
+ * Prints: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}

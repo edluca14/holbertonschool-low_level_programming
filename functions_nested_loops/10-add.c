f15da743cfa6d3d152d4ca83da75b029db08ee2f
edluca14 <6755@holbertonstudents.com>
2023-06-19 05:54:50 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds the two variables and returns the result
+ *
+ * @a: variable, check the number
+ * @b: variable, check the number
+ *
+ * Return: returns the result of adding a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

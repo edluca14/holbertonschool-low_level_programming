92a57076a9ecc5f5c6189b992851de18fdc9cd8e
edluca14 <6755@holbertonstudents.com>
2023-06-19 12:08:42 -0700

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies the variables a and b.
+ *
+ * @a: variable.
+ * @b: variable.
+ *
+ * Return: the multiply between a and b.
+ *
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}

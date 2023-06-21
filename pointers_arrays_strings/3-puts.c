da5c9e5d75ace0f48c34af1417e07117e334dd4c
edluca14 <6755@holbertonstudents.com>
2023-06-21 11:56:14 -0700

    task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string, followed by a new line, to stdout.
  *
@@ -9,8 +10,8 @@ void _puts(char *str)
 {
 	while (*str != 0)
 	{
-		_putchar (*str);
+		putchar (*str);
 		str++;
 	}
-	_putchar ('\n');
+	putchar ('\n');
 }

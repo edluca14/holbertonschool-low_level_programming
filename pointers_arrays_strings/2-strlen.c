380e117f25354c81da4d292955debc5ffe22a9c9
edluca14 <6755@holbertonstudents.com>
2023-06-21 11:20:07 -0700

    task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * strlen - returns the length of a string.
+ * _strlen - returns the length of a string.
  *
  * @s: parameter.
  *
+ * Return: the number of increases of contador until *s hits 0
  */
 int _strlen(char *s)
 {
-	int contador= 0;
+	int contador = 0;
 
 	while (*s != 0)
 	{

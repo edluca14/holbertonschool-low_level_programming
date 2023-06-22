74ae6e2453fb8e21887394ea2e2bb964a164dd4c
edluca14 <6755@holbertonstudents.com>
2023-06-21 19:56:27 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != 0)
+	{
+		count++;
+	}
+	while (count >= 0)
+	{
+		count--;
+		_putchar (s[count]);
+	}
+	_putchar ('\n');
+}

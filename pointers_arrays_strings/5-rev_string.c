09ff7304b868685561faa13c6250b1f62add4a50
edluca14 <6755@holbertonstudents.com>
2023-06-22 07:11:09 -0700

    task5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 /**
+ * rev_string - write a function that reverses a string
  *
+ * @s: parameter.
  *
  */
 void rev_string(char *s)
 {
-	int count = 0;
+	int tamano = strlen(s), i;
+	char auxiliar;
 
-	while (*s != 0)
+	for (i = 0; i<tamano/2; i++)
 	{
-		_putchar (*s);
-		s++;
-	}
-	_putchar ('\n');
-
-	while (s[count] != 0)
-	{
-		count++;
-	}
-	while (count > 0)
-	{
-		count--;
-		_putchar (s[count]);
+		auxiliar = s[i];
+		s[i] = s[tamano-1-i];
+		s[tamano-i-1] = auxiliar;
 	}
 }

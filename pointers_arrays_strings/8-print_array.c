165d8da23ea16684221f90de5e3c1f34ee2356b4
edluca14 <6755@holbertonstudents.com>
2023-06-22 18:15:15 -0700

    task8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "mail.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers, followed by a new
@@ -10,7 +10,7 @@ void print_array(int *a, int n)
 {
 	int contador;
 
-	for (contador = 0; contador < n: contador++)
+	for (contador = 0; contador < n; contador++)
 	{
 		printf("%d", a[contador]);
 		if (contador == n - 1)
@@ -19,5 +19,5 @@ void print_array(int *a, int n)
 		}
 		printf(", ");
 	}
-	printf("\n"):
+	printf("\n");
 }

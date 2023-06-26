d9da3080f2cb0ccca42918104b600ffa8ae03b64
edluca14 <6755@holbertonstudents.com>
2023-06-26 15:08:59 -0700

    no entiendo nati

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
+ *
  * _memset - Write a function that fills memory with a constant byte.
  *
  * @s: parameter
  * @b: parameter
  * @n: parameter
  *
+ * Return: s (Successful)
  */
 char *_memset(char *s, char b, unsigned int n)
 {

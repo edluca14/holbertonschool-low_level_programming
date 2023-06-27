88b632f97b7d4503ee50cd939fb706b6aa2f1262
edluca14 <6755@holbertonstudents.com>
2023-06-27 07:11:04 -0700

    setso tilin

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The main string to search in.
+ * @needle: The substring to find.
+ *
+ * Return: Pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		return (haystack);
+	while (*haystack)
+	{
+		int i = 0;
+		while (haystack[i] == needle[i])
+		{
+			if (needle[i + 1] == '\0')
+				return (haystack);
+			i++;
+		}
+		haystack++;
+	}
+	return (NULL);
+}

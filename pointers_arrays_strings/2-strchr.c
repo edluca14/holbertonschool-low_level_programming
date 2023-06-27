fc20ab88f91d689fbef1d7998ba70fb5c3cab9f2
edluca14 <6755@holbertonstudents.com>
2023-06-26 17:07:08 -0700

    eso tilin wow tilin a la mrd tilin

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,12 @@
 #include "main.h"
 /**
+ * _strchr - write a function that locates a character in a string.
+ *
+ * @s: parameter.
+ * @c: parameter.
+ *
+ * Return: the pointer to the first occurence of c in the string, NULL
+ * otherwise.
  *
  */
 char *_strchr(char *s, char c)
@@ -7,11 +14,11 @@ char *_strchr(char *s, char c)
 	int count;
 
 	for (count = 0; s[count] != c && s[count] != '\0'; count++);
-	
+
 	if (s[count] == c)
 	{
 		return (s + count);
 	}
-	
+
 	return ('\0');
 }

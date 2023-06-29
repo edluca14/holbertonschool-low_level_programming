47e96686a535e2304d4cb524033ad303cf1085cf
edluca14 <6755@holbertonstudents.com>
2023-06-28 18:07:57 -0700

    ntus ptus

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,12 +4,14 @@
  * _strlen_recursion - write a function that returns the length of a string.
  *
  * @s: parameter that contains the string.
+ *
+ * Return: the length of the string.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{

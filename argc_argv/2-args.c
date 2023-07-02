1c8a0d777c2121ed9c3e0ced91ddab7726e74cf7
edluca14 <6755@holbertonstudents.com>
2023-07-02 12:06:30 -0700

    matysitoflowbakan0

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
+ * main - Write a program that prints all arguments it receives.
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
 {

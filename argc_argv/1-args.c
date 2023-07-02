bcd57bc77143c39d810001d21124772861d74bfc
edluca14 <6755@holbertonstudents.com>
2023-07-02 07:32:21 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that prints the number of arguments passed into it.
+ *
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: 0 (Succeess)
+ */
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

ee3cb2ab73694206627e2df03d724113e3535f37
edluca14 <6755@holbertonstudents.com>
2023-07-02 11:39:34 -0700

    all hail neter el peter

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,13 +4,14 @@
  * main - Write a program that prints the number of arguments passed into it.
  *
  * @argc: parameter
- * @argv: parameter
  *
  * Return: 0 (Succeess)
  */
 int main (int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	argv++;
 
-	return (0);
+	printf("%d\n", argc -1);
+
+	return 0;
 }

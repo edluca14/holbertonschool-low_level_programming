01978b817286f08ecd3bed2bf58bf153bf5608e5
edluca14 <6755@holbertonstudents.com>
2023-07-02 07:19:58 -0700

    corrections

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 /**
  * main - Write a program that prints its name, followed by a new line.
- *
+ * @argc: parameter
+ * @argv: parameter
  * Return: 0 (Success).
  */
-int main (int argc, char* argv[])
+int main (int argc, char *argv[])
 {
 	int i;
 	for (i = 0; i < argc; i++)

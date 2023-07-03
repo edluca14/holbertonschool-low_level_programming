f47d85347178923ce3250ca9c325387a1f0707b5
edluca14 <6755@holbertonstudents.com>
2023-07-02 20:50:06 -0700

    sajksja

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Write a program that finds and prints the largest prime factor of the
+ * number
+ *
+ * Return: 0 (Successful)
+ */
+int main()
+{
+	long int n = 612852475143;
+	int i;
+	int neurona = 1;
+
+	for (i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			if (neurona < i)
+			{
+				neurona = i;
+			}
+			n /= i;
+		}
+	}
+	if (n > neurona)
+	{
+		neurona = n;
+	}
+
+	printf("%d\n", neurona);
+	return (0);
+}
